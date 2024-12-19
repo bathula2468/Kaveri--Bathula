1acf7ce70bead100aa4ffd4c7916af7df4c31198
Kaveri Bathula <kaveribathula74@gmail.com>
2024-12-19 21:31:01 +0530

    Add files via upload

---
+++ Python Lists.py
@@ -0,0 +1,7 @@
+numbers=[10,0,-1,7,8,10,-67]
+#names=["Rathna Kumari","Sriramulu","Kaveri","Amos"]
+#Mix_List=[1,"Kaveri",True,7.5
+numbers.remove(0)
+print(numbers)
+#print(names)
+#print(Mix_List)
